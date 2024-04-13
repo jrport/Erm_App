4a54af003c2539a518cd0602c55a42d4a06b1e3b
jrport <joao6roberto@gmail.com>
2024-04-12 23:28:46 -0300

    Start of add page for pedidos and items

--- app/controllers/pedidos_controller.rb
+++ app/controllers/pedidos_controller.rb
@@ -1,2 +1,17 @@
 class PedidosController < ApplicationController
+  def index
+    @pedidos = Pedido.all
+    @lojas = Loja.all
+  end
+
+  def show
+    pedido_id = params[:id]
+    @pedido = Pedido.find(pedido_id)
+    @loja = @lojas.find(@pedido.loja_id)
+    @items = Item.where(pedido_id: pedido_id)
+  end
+
+  def new
+    @lojas = Loja.all
+  end
 end
